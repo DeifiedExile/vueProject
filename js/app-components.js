5ae0157fb83aec6347c34a2c6c48749d990f29c2
Lucas <lwolfs@my.wctc.edu>
2019-09-30 10:49:26 -0500

    Added title. Added textured background. Adjusted styling for eoa

--- js/app-components.js
+++ js/app-components.js
@@ -1,6 +1,6 @@
 Vue.component('mtg-card-list', {
     props: {
-        name:{
+        name: {
             type: String,
             default: 'Card'
         },
@@ -11,7 +11,7 @@ Vue.component('mtg-card-list', {
     },
 
     methods: {
-        add: function(card){
+        add: function (card) {
             card.qty++;
         },
 
@@ -24,17 +24,17 @@ Vue.component('mtg-card-list', {
         }
     },
 
-    template:`
+    template: `
 <div class="cardListDisplay col-lg-12">
     
     <ul class="list-group list-inline list-group-flush col-lg-12 row">
         <div class="row" v-for="i in Math.ceil(cards.length / 4)">
                 <li v-for="(card, i) in cards.slice((i - 1) * 4, i * 4)" :key="card.name" class="list-group-item col-md-3 text-center">
                     <div class="card mtgCard" v-bind:class="card.color.toLowerCase()">
-                        <div class="card-body">
+                        <div class="card-body mtgCardBody">
                             <h4 class="card-title info rounded col-md-auto" >{{card.name}}</h4>
                             <br/>
-                            <h6 class="card-subtitle info text-muted col-6 offset-3" >{{card.type}}</h6>
+                            <h6 class="card-subtitle info col-6 offset-3" >{{card.type}}</h6>
                             <br/>
                             <p class="card-text info col-6 offset-3"> Quantity: {{card.qty}} </p>
                             <button class="btn btn-tiny" @click="add(card)"><i class="fas fa-plus-circle"></i></button>
@@ -45,12 +45,5 @@ Vue.component('mtg-card-list', {
         </div>
     </ul>
 </div>`,
-    computed: {
-
-    }
-
-
-    });
-
-
-// 'color': card.color, '-webkit-filter': 'invert(10%)'
+    computed: {}
+});
--- js/app.js
+++ js/app.js
@@ -1,135 +1,112 @@
 var app = new Vue({
-   el: '#app',
-   data: {
-       newCard: {
-           name: '',
-           qty: 1,
-           type: 'Land',
-           color: 'colorless'
-       },
-       filterOptions: {
-         color: '',
-         type: ''
-       },
-       cardList: [
-           {name: 'Forest', qty: 20, type: 'Land', color: 'silver'},
-           {name: 'Island', qty: 20, type: 'Land', color: 'silver'},
-           {name: 'Mountain', qty: 20, type: 'Land', color: 'silver'},
-           {name: 'Swamp', qty: 20, type: 'Land', color: 'silver'},
-           {name: 'Plains', qty: 20, type: 'Land', color: 'silver'},
+    el: '#app',
+    data: {
+        newCard: {
+            name: '',
+            qty: 1,
+            type: 'Land',
+            color: 'colorless'
+        },
+        filterOptions: {
+            color: '',
+            type: ''
+        },
+        cardList: [
+            {name: 'Forest', qty: 20, type: 'Land', color: 'silver'},
+            {name: 'Island', qty: 20, type: 'Land', color: 'silver'},
+            {name: 'Mountain', qty: 20, type: 'Land', color: 'silver'},
+            {name: 'Swamp', qty: 20, type: 'Land', color: 'silver'},
+            {name: 'Plains', qty: 20, type: 'Land', color: 'silver'},
 
-           {name: 'Grizzly Bears', qty: 4, type: 'Creature', color: 'Green'},
-           {name: 'Sun Titan', qty: 2, type: 'Creature', color: 'White'},
-           {name: 'Solemn Simulacrum', qty: 4, type: 'Creature', color: 'silver'},
-           {name: 'Blood Artist', qty: 4, type: 'Creature', color: 'Black'},
-           {name: 'Mulldrifter', qty: 4, type: 'Creature', color: 'Blue'},
-           {name: 'Raging Goblin', qty: 4, type: 'Creature', color: 'Red'},
+            {name: 'Grizzly Bears', qty: 4, type: 'Creature', color: 'Green'},
+            {name: 'Sun Titan', qty: 2, type: 'Creature', color: 'White'},
+            {name: 'Solemn Simulacrum', qty: 4, type: 'Creature', color: 'silver'},
+            {name: 'Blood Artist', qty: 4, type: 'Creature', color: 'Black'},
+            {name: 'Mulldrifter', qty: 4, type: 'Creature', color: 'Blue'},
+            {name: 'Raging Goblin', qty: 4, type: 'Creature', color: 'Red'},
 
-           {name: 'Counterspell', qty: 2, type: 'Instant', color: 'Blue'},
-           {name: 'Swords to Plowshares', qty: 2, type: 'Instant', color: 'White'},
-           {name: 'Beast Within', qty: 2, type: 'Instant', color: 'Green'},
-           {name: 'Chaos Warp', qty: 2, type: 'Instant', color: 'Red'},
-           {name: 'Vampiric Tutor', qty: 2, type: 'Instant', color: 'Black'},
+            {name: 'Counterspell', qty: 2, type: 'Instant', color: 'Blue'},
+            {name: 'Swords to Plowshares', qty: 2, type: 'Instant', color: 'White'},
+            {name: 'Beast Within', qty: 2, type: 'Instant', color: 'Green'},
+            {name: 'Chaos Warp', qty: 2, type: 'Instant', color: 'Red'},
+            {name: 'Vampiric Tutor', qty: 2, type: 'Instant', color: 'Black'},
 
 
-           {name: 'Pacifism', qty: 1, type: 'Enchantment', color: 'White'},
-           {name: 'Rhystic Study', qty: 1, type: 'Enchantment', color: 'Blue'},
-           {name: 'Phyrexian Arena', qty: 1, type: 'Enchantment', color: 'Black'},
-           {name: 'Sylvan Library', qty: 1, type: 'Enchantment', color: 'Green'},
-           {name: 'Warstorm Surge', qty: 1, type: 'Enchantment', color: 'Red'},
+            {name: 'Pacifism', qty: 1, type: 'Enchantment', color: 'White'},
+            {name: 'Rhystic Study', qty: 1, type: 'Enchantment', color: 'Blue'},
+            {name: 'Phyrexian Arena', qty: 1, type: 'Enchantment', color: 'Black'},
+            {name: 'Sylvan Library', qty: 1, type: 'Enchantment', color: 'Green'},
+            {name: 'Warstorm Surge', qty: 1, type: 'Enchantment', color: 'Red'},
 
-           {name: 'Duress', qty: 2, type: 'Sorcery', color: 'Black'},
-           {name: 'Cultivate', qty: 2, type: 'Sorcery', color: 'Green'},
-           {name: 'Blasphemous Act', qty: 2, type: 'Sorcery', color: 'Red'},
-           {name: 'Ponder', qty: 2, type: 'Sorcery', color: 'Blue'},
-           {name: 'Wrath of God', qty: 2, type: 'Sorcery', color: 'White'},
+            {name: 'Duress', qty: 2, type: 'Sorcery', color: 'Black'},
+            {name: 'Cultivate', qty: 2, type: 'Sorcery', color: 'Green'},
+            {name: 'Blasphemous Act', qty: 2, type: 'Sorcery', color: 'Red'},
+            {name: 'Ponder', qty: 2, type: 'Sorcery', color: 'Blue'},
+            {name: 'Wrath of God', qty: 2, type: 'Sorcery', color: 'White'},
 
-           {name: 'Jaya Ballard', qty: 2, type: 'Planeswalker', color: 'Red'},
-           {name: 'Elspeth, Sun\'s Champion', qty: 2, type: 'Planeswalker', color: 'White'},
-           {name: 'Tezzeret the Seeker', qty: 2, type: 'Planeswalker', color: 'Blue'},
-           {name: 'Liliana Vess', qty: 2, type: 'Planeswalker', color: 'Black'},
-           {name: 'Nissa, Vital Force', qty: 2, type: 'Planeswalker', color: 'Green'},
+            {name: 'Jaya Ballard', qty: 2, type: 'Planeswalker', color: 'Red'},
+            {name: 'Elspeth, Sun\'s Champion', qty: 2, type: 'Planeswalker', color: 'White'},
+            {name: 'Tezzeret the Seeker', qty: 2, type: 'Planeswalker', color: 'Blue'},
+            {name: 'Liliana Vess', qty: 2, type: 'Planeswalker', color: 'Black'},
+            {name: 'Nissa, Vital Force', qty: 2, type: 'Planeswalker', color: 'Green'},
 
-           {name: 'Karn, Silver Golem', qty: 1, type: 'Artifact', color: 'silver'},
-           {name: 'Sol Ring', qty: 1, type: 'Artifact', color: 'silver'},
-           {name: 'Lightning Greaves', qty: 1, type: 'Artifact', color: 'silver'},
-           {name: 'Skullclamp', qty: 1, type: 'Artifact', color: 'silver'},
+            {name: 'Karn, Silver Golem', qty: 1, type: 'Artifact', color: 'silver'},
+            {name: 'Sol Ring', qty: 1, type: 'Artifact', color: 'silver'},
+            {name: 'Lightning Greaves', qty: 1, type: 'Artifact', color: 'silver'},
+            {name: 'Skullclamp', qty: 1, type: 'Artifact', color: 'silver'},
 
-       ],
-   },
+        ],
+    },
     // components: {
     //    mtg-card-list: mgt-card-list
     // },
     methods: {
-       addCard: function(){
-           this.cardList.push(this.newCard);
-           this.newCard = {
-               name: '',
-               qty: 1,
-               type: 'Land',
-               color: 'silver'
-           };
-           $('#addCardModal').modal('hide');
+        addCard: function () {
+            this.cardList.push(this.newCard);
+            this.newCard = {
+                name: '',
+                qty: 1,
+                type: 'Land',
+                color: 'silver'
+            };
+            $('#addCardModal').modal('hide');
 
-       },
-        removeCard(card){
-           this.cardList.splice(this.cardList.indexOf(card), 1);
         },
-
-        // bgColor: function(card){
-        //    switch(card.color.toUpperCase())
-        //    {
-        //        case 'WHITE':
-        //            return 'white';
-        //        case 'BLUE':
-        //            return 'rgb(102, 119, 153)';
-        //        case 'BLACK':
-        //            return 'rgb(32, 32, 32)';
-        //        case 'GREEN':
-        //            return 'rgb(122, 148, 96)';
-        //        case 'RED':
-        //            return 'rgb(119, 31, 31)';
-        //        default:
-        //            return 'rgb(129, 149, 169)';
-        //
-        //    }
-        //
-        // }
-
-
-
+        removeCard(card) {
+            this.cardList.splice(this.cardList.indexOf(card), 1);
+        },
     },
     computed: {
-        getCards: function(){
+        getCards: function () {
             var colorChoice = this.filterOptions.color.toString();
             var typeChoice = this.filterOptions.type.toString();
-            return this.cardList.filter(function(card){
+            return this.cardList.filter(function (card) {
                 return card.color.toUpperCase().includes(colorChoice.toUpperCase()) && card.type.toUpperCase().includes(typeChoice.toUpperCase());
             });
         },
 
 
     },
-    mounted: function() {
-       if(localStorage.getItem('cardList')){
-           this.cardList = JSON.parse(localStorage.getItem('cardList'));
-       }
+    mounted: function () {
+        if (localStorage.getItem('cardList')) {
+            this.cardList = JSON.parse(localStorage.getItem('cardList'));
+        }
     },
     watch: {
-       filterOptions: {
-           handler: function(){
+        filterOptions: {
+            handler: function () {
 
-           }
+            }
 
-       },
-        deep:true,
+        },
+        deep: true,
 
 
-       cardList: {
-           handler: function(newList){
-               localStorage.setItem('cardList', JSON.stringify(newList));
-           },
-           deep: true
-       }
+        cardList: {
+            handler: function (newList) {
+                localStorage.setItem('cardList', JSON.stringify(newList));
+            },
+            deep: true
+        }
     }
 });
